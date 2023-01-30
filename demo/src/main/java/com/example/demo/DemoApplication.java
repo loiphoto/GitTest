634060544c4f1d13559bbe781d86e37a07004e56
Loi Nguyen Duc <loi.nguyen1@ntq-solution.com.vn>
2023-01-30 17:22:38 +0900

    b

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("a");
         System.out.println("b");
+        System.out.println("c");
     }
 
 }
